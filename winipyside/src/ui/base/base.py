975da1175cab4f3b43855e52dac9bc32d224a1b7
Winipedia <winipedia@gmx.de>
2025-12-22 09:40:23 +0100

    update fix

--- winipyside/src/ui/base/base.py
+++ winipyside/src/ui/base/base.py
@@ -11,7 +11,7 @@ from typing import TYPE_CHECKING, Any, Self, cast
 from pyrig.src.modules.class_ import (
     get_all_nonabstract_subclasses,
 )
-from pyrig.src.modules.package import walk_package
+from pyrig.src.modules.imports import walk_package
 from pyrig.src.resource import get_resource_path
 from pyrig.src.string import split_on_uppercase
 from PySide6.QtCore import QObject
