e6aeb5390e1c5b274966235c5a926224fbaf596a
Winipedia <winipedia@gmx.de>
2026-07-11 06:39:49 +0100

    pyrig upgrades

--- src/winipyside/rig/tools/project_tester.py
+++ src/winipyside/rig/tools/project_tester.py
@@ -1,6 +1,6 @@
 """Override pyrig's ProjectTester to add custom dev dependencies."""
 
-from pyrig.rig.tools.testers.project import ProjectTester as BaseProjectTester
+from pyrig.rig.tools.testing.project import ProjectTester as BaseProjectTester
 
 
 class ProjectTester(BaseProjectTester):
